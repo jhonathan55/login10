dcf8717c98ffd17aad6987f68818de46b0344b8a
jhonathan55 <jfgutierrezsaico@gmail.com>
2024-05-20 22:07:07 -0400

    interfaz login

--- app/src/main/java/com/example/login10/view/login/LoginFragment.kt
+++ app/src/main/java/com/example/login10/view/login/LoginFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.login10.R
-import com.example.login10.databinding.FragmentHomeBinding
 import com.example.login10.databinding.FragmentLoginBinding
 
 
@@ -21,6 +21,21 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Navegar al HomeFragment al hacer clic en el botón de login
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
+        // Navegar al RegisterFragment al hacer clic en el botón de register
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
